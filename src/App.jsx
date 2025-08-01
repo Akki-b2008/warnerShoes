23445a7f8d46bdfc72bd1d3050db4fff029f9a73
Akki-b2008 <rathoreakash2356@gmail.com>
2025-08-01 23:47:25 +0530

    vercel error fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useLocation } from "react-router";
 import ExpandableTabs from "./components/ExpandTabs/ExpandableTabs"
-import Nav from "./components/Navbar/nav";
+import Nav from "./components/Navbar/Nav";
 import Mainroutes from "./routes/Mainroutes"
 
 const App = () => {
