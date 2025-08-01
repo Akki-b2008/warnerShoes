c37153d9cbf35722d2ec1a07c006de82a0e420b2
Akki-b2008 <rathoreakash2356@gmail.com>
2025-08-02 00:24:08 +0530

    Fix: correct case for Home.jsx to fix Linux build error

--- src/routes/MainRoutes.jsx
+++ src/routes/MainRoutes.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Route, Routes } from 'react-router'
-import Home from '../pages/home/Home';
+import Home from '../../src/pages/home/Home';
 import Auth from '../pages/AuthPage/Auth'
 import Shop from '../pages/Shop/Shop'
 import AboutUs from '../pages/AboutPage/AboutUs'
